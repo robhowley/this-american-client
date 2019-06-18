99177133579012898d1e3d4a72804091a41b063f
robhowley <howley.robert@gmail.com>
2019-06-17 23:11:54 -0400

    restrict version

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
-
+from os import path
 from setuptools import setup, find_packages
 
 
 package_name = 'thisamericanlife'
 tal_module_ref = __import__(package_name)
 
+# read the README
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name=package_name,
     version=tal_module_ref.__version__,
     description='scraper client for the This American Life website',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author_email=tal_module_ref.__author_email__,
     url='https://github.com/{package_name}',
     packages=find_packages(exclude=['tests']),
-    install_requires=['lxml']
+    install_requires=['lxml'],
+    python_requires='>=3.6.0'
 )
